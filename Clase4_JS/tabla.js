2c678ddc78de7521852595ef6da8dd3ba7cabed3
alumno <alumno>
2017-09-26 22:08:09 -0300

    EnvioYRecepcionDatosConSErver

--- Clase4_JS/tabla.js
+++ Clase4_JS/tabla.js
@@ -9,11 +9,15 @@ var txtNombre=document.getElementById("Nombre").value;
 var txtApellido=document.getElementById("Apellido").value;
 //confirm("Confirmo que deseo agregar un usuario");
 
+
+
+
 if(txtNombre=="")
     {
         
      document.getElementById("Nombre").style.borderColor = "red";
      alert("Debe ingresar un Nombre");
+   
      
     
     }
@@ -32,11 +36,59 @@ if(txtNombre=="Federico" && txtApellido=="Tomadin")
 }
 
 
-var tcuerpo = document.getElementById("tablaUsuarios");   
-    tcuerpo.innerHTML = tcuerpo.innerHTML+"<td>"+txtNombre+"</td>"+"<td>"+txtApellido+"</td>"
+//var objJSON = { "nombre": txtNombre, "apellido=":txtApellido};
+var obj ='nombre=' + encodeURIComponent(txtNombre) + '&apellido=' + encodeURIComponent(txtApellido);
+//datos=JSON.stringify("nombre " + txtNombre + "Edad" + txtApellido);
+xhr=new XMLHttpRequest();
+xhr.onreadystatechange=gestionarRespuesta ;
+xhr.open('POST','http://localhost:3000/agregarpersona',true);
+xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+//xhr.send(JSON.stringify(objJSON));
+xhr.send(obj);
 
+
+
+
+
+
+
+var tcuerpo = document.getElementById("tablaUsuarios");   
+tcuerpo.innerHTML = "";
+    tcuerpo.innerHTML = tcuerpo.innerHTML+
+    "<tr><td>"+txtNombre+"</td><td>"+txtApellido +
+    "</td><td><button type='button' id='btnEliminar' onclick = eliminar( " + i + ")>Eliminar</button>"
+    + "<button id='btnModificar' type='button'>Modificar</button>"+"</td>"+"</tr>";
+
+    
 })
+
+
+function gestionarRespuesta()
+{
+    if(xhr.readyState==4) //4 quiere decir que lo puedo enviar y lo recibio
+    {
+        if(xhr.status==200)
+        {
+        alert('Envio exitoso');
+        }
+    
+        else {
+         div.innerHTML="Envio Exitoso";
+        }
+    }
+}
+
+function eliminar(index){
+    alert("aca voy a borrar el indice " + index) ;
+}
+
+
+
+
+
+
 
 
 
 }
+

ed1b0011264a52086b3aad81b5daf842a969e81b
federicotomadin <federicotomadin@gmail.com>
2017-11-28 02:11:07 -0300

    segundo parcial

---
+++ federico.tomadin.3D/controladora.js
@@ -0,0 +1,12 @@
+"use strict";
+/// <reference path="Animal.ts" />
+/// <reference path="Mascota.ts" />
+$(document).ready(function () {
+    alert("Bienvenido al sistema");
+    Controladora.CargarSelect();
+});
+var Controladora = /** @class */ (function () {
+    function Controladora() {
+    }
+    return Controladora;
+}());
---
+++ federico.tomadin.3D/controladora.ts
@@ -0,0 +1,18 @@
+
+/// <reference path="Animal.ts" />
+/// <reference path="Mascota.ts" />
+
+
+
+$(document).ready(function () {
+    
+    
+        alert("Bienvenido al sistema");
+        Controladora.CargarSelect();
+    });
+    
+    
+    class Controladora 
+    {
+        
+    }    

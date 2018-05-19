a22ff4db6dc85c2261bb31f8def8c62a6249e02b
federicotomadin <federicotomadin@gmail.com>
2018-05-19 18:19:24 -0300

    se paso todas las funcionalidades a jquery y se agrego ventana modal

--- Tomadin_Federico_3d_PasarJquery/js/manejador.js
+++ Tomadin_Federico_3d_PasarJquery/js/manejador.js
@@ -3,193 +3,138 @@ var restoredPersonas;
 
 
 function TraerPersonas(){
-    xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = gestionarRespuestaTraerPersonas;
-    xhr.open('GET',"http://localhost:3000/notas", true);
-    spinner.hidden=false;
-    xhr.send();
-}
-//Respuesta al TraerPersonas
-function gestionarRespuestaTraerPersonas() {
 
-    var div = document.getElementById('respuesta');
+   $.ajax({
+    url: 'http://localhost:3000/notas',
+    type: 'get',
+    dataType: 'json',
+    beforeSend: function(){
+        $('#spinner').show();
+    },
+    success: function(response)
+    {
+        $('#spinner').hide();
+        localStorage.setItem('personas',JSON.stringify(response));       
+        CargarTabla(response);
+    },
+    error:function(jqXHR, textStatus, textError){ alert("error!!" + textStatus + textError)}
+   })
 
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-            //Si esta todo bien, guardo la respuesta del server
-            spinner.hidden=true;
-            var restoredPersonas = JSON.parse(xhr.responseText);
-            localStorage.setItem('personas',JSON.stringify(restoredPersonas));
-          // localStorage.setItem({'Nombre' : json.stringify(objPersonas.nombre)}, JSON.stringify(objPersonas.apellido) );
-            //llamo a mi función que dibujará la tabla.
-            CargarTabla(restoredPersonas);
-            
-        }
-        else {
-
-            div.innerHTML = "Error: " + xhr.status + " " + xhr.statusText;
-        }
-    }
-   
 }
 
 function Guardar() {
     
         //Tomo los valores del HTML
-        var id= localStorage.getItem('id')
-        var legajo = document.getElementById('txtLegajo').value;
-        var nombre = document.getElementById('txtNombre').value;
-        var materia = document.getElementById('txtMateria').value;
-        var nota = document.getElementById('txtNota').value;
+        var id= localStorage.getItem('id');
 
-        var alumno = { "id": id, "legajo": legajo,
-        "nombre": nombre, "materia": materia,
-        "nota": nota };
+        var alumno = { "id": id, "legajo": $('#txtLegajo').val(),
+        "nombre": $('#txtNombre').val(), "materia": $('#txtMateria').val(),
+        "nota":  $('#txtNota').val()};
+   
+        var indice = $('#hdIndice').val();
     
-        var indice = document.getElementById('hdIndice').value;
-      
-    
-            xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = gestionarRespuestaGuardar;
-            xhr.open('POST', "http://localhost:3000/editarNota", true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            //Objeto transformando a JSON   
-            spinner.hidden=false;
-            xhr.send(JSON.stringify(alumno));      
+        $('#spinner').show();
+        $.ajax({
+        url: 'http://localhost:3000/editarNota',
+        data: alumno,
+        method: 'post',
+        dataType: 'json',
+        success: function(response)
+        { 
+        TraerPersonas();
+        $('#hdLegajo').val() = "";
+        $('#txtNombre').val() = "";
+        $('#txtMateria').val() = "";
+        $('#txtNota').val() = "";
+        $('#spinner').hide();
+        }
+
+        })
+        
+        $('.vgVentana').hide();
            
     }
-
-    function gestionarRespuestaGuardar()
-    {
-        var div = document.getElementById('respuesta');
-        
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    div.innerHTML = xhr.responseText;
-                    //si esta todo bien refresco la lista de personas
-                    spinner.hidden=true;
-                    TraerPersonas();
-                    //Limpio las caja de texo y el indice
-                    document.getElementById('hdLegajo').value = "";
-                    document.getElementById('txtNombre').value = "";
-                    document.getElementById('txtMateria').value = "";
-                    document.getElementById('txtNota').value = "";
-                               
-                } else {
-        
-                    div.innerHTML = "Error: " + xhr.status + " " + xhr.statusText;
-                }
-            } else {
-                div.innerHTML = '<span>Algo esta pasando que no guarda los datos</span>';
-            }
-    
-    }
-
 
 
 function TraerPersona(indice) {
     var objetoJSON= JSON.parse(localStorage.getItem('personas'));
-  //  IndiceGlobal= indice;
   
     localStorage.setItem('id',JSON.stringify(objetoJSON[indice].id));
-    document.getElementById('txtLegajo').value =objetoJSON[indice].legajo;
-    document.getElementById('txtNombre').value = objetoJSON[indice].nombre;
-    document.getElementById('txtMateria').value =objetoJSON[indice].materia;
-    document.getElementById('txtNota').value = objetoJSON[indice].nota;
-  
+    $('#txtLegajo').val(objetoJSON[indice].legajo); 
+    $('#txtNombre').val(objetoJSON[indice].nombre);
+    $('#txtMateria').val(objetoJSON[indice].materia);
+    $('#txtNota').val(objetoJSON[indice].nota);
+
+    $('.vgVentana').show();
+    
   }
 
-
 function LimpiarTabla(){
-    var tCuerpo = document.getElementById("tCuerpo").innerHTML="";
-        }
+
+    var tCuerpo = $("#tCuerpo").html("");
+                      }
 
 
     function CargarTabla(restoredPersonas){
         
-            //Primero limpio la tabla
             LimpiarTabla();
-            //Luego la dibujo nuevamente
-            var tCuerpo = document.getElementById("tCuerpo");      
+            var body=""; 
         
             for(i = 0; i < restoredPersonas.length; i++ ){
                 
                 if(restoredPersonas[i].nota < 4)
                 {
-                    tCuerpo.innerHTML = tCuerpo.innerHTML +
-                    "<td style= 'color : red'>" + restoredPersonas[i].legajo + "</td>" +
+                   var cadena=
+                    "<tr><td style= 'color : red'>" + restoredPersonas[i].legajo + "</td>" +
                     "<td style= 'color : red'>" + restoredPersonas[i].nombre + "</td>" +
                     "<td style= 'color : red'>" + restoredPersonas[i].materia + "</td>" +
                     "<td style= 'color : red'>" + restoredPersonas[i].nota + "</td>" +
-                    "<td><input id='btnModificar' class='btn btn-primary' type='button' value='Editar' onclick='TraerPersona(" + i + ");'/></td>"
-            
+                    "<td><input id='btnModificar' class='btn btn-primary' type='button' value='Editar' onclick='TraerPersona(" + i + ");'/></td></tr>"
+                    body+=cadena;  
                 }
         
-                tCuerpo.innerHTML = tCuerpo.innerHTML +
-                "<td>" + restoredPersonas[i].legajo + "</td>" +
+               var cadena2=
+                "<tr><td>" + restoredPersonas[i].legajo + "</td>" +
                 "<td>" + restoredPersonas[i].nombre + "</td>" +
                 "<td>" + restoredPersonas[i].materia + "</td>" +
                 "<td>" + restoredPersonas[i].nota + "</td>" +
-                "<td><input id='btnModificar' class='btn btn-primary' type='button' value='Editar' onclick='TraerPersona(" + i + ");'/></td>";
-                
-            
-        }
+                "<td><input id='btnModificar' class='btn btn-primary' type='button' value='Editar' onclick='TraerPersona(" + i + ");'/></td></tr>"
+                body+=cadena2;     
+            }
+            $("#tCuerpo").html(body);
+
     }
 
 function Cerrar()
 {
-    document.getElementById("contenedor").hidden=true;
+    $('.vgVentana').hide();
 
 }
 
 
-window.onload = function()
+$(document).ready(function()
 {
-     //Boton guardar
-     var btnEnviar = document.getElementById("btnEnviar");
-  //   Seteo un evento al btnGuardar
-     btnGuardar.addEventListener('click',function(){
-         Guardar();   
-        })  
+     $('#btnGuardar').click(Guardar)
 
-        btnCerrar.addEventListener('click',function(){
-            Cerrar();   
-           }) 
+    $('#btnCerrar').click(Cerrar)
 
-    var spinner= document.getElementById('spinner').innerHTML="<img src='spinner.gif'>";
+
+    $('#spinner').html("<img src='spinner.gif'>");
+    $('#spinner').hide();
+    $('.vgVentana').hide();
     
     var usuario = localStorage.getItem('usuario');
   
-    if(usuario=='Admin')  
+    if(usuario==='Admin')  
     {
-        document.getElementById("contenedor").hidden=false;
+        $('#spinner').hide();
+        $("#contenedor").show();
         TraerPersonas();
         return;
     }
 
     TraerPersonas();
-    document.getElementById('contenedor').hidden=true;
+    $("#contenedor").hide();
    
-
-
-    /* var btnEliminar = document.getElementById("btnEliminar");
-     btnEliminar.addEventListener('click', function(){
-         EliminarPersona(indice);
-     })     */
-    
-    //Cuando carga la pagina TRAIGO TODAS LAS PERSONAS
-
-    //var mydata = JSON.parse(data);
-    //localStorage.setItem('personas', data);
-
-    //restoredPersonas = JSON.parse(notas);
-    //CargarTabla(restoredPersonas);
-    //TraerPersonas();
-
- 
-    //localStorage.getItem(personas)
-   // var json= JSON.parse( '{"nombre" : "matias" }' );
-  //  localStorage.setItem('personas', JSON.stringify(json));
- 
-}
+})
 
--- Tomadin_Federico_3d_PasarJquery/js/manejadorLogin.js
+++ Tomadin_Federico_3d_PasarJquery/js/manejadorLogin.js
@@ -4,88 +4,47 @@ var emailGlobal;
 
 function ValidarUsuario() {
 
-    //Tomo los valores del HTML
-    var email = document.getElementById('txtEmail').value;
-    var contrasenia = document.getElementById('txtContrasenia').value;
+        var persona = {  "email": $('#txtEmail').val(), "password": $('#txtContrasenia').val() };
+        emailGlobal=$('#txtEmail').val();
 
-  //   spinner.innerHTML="<img src='spinner.gif'>";
-    //spinner.innerHTML="<img src='spinner.gif'>"
-
-    
-  
-      
-        var persona = {  "email": email, "password": contrasenia };
-        emailGlobal= email;
-        
-
-        /*otra opcion*/
-        //var data = "nombre=" + encodeURIComponent(nombre) + "&apellido=" + encodeURIComponent(apellido);
-
-
-        xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = gestionarValidarUsuario;
-        xhr.open('POST', "http://localhost:3000/login", true);
-        xhr.setRequestHeader("Content-type", "application/json");
-        
-     
-       
-
-        //Objeto transformando a JSON
-       // var data = "personas=" + JSON.stringify(persona);
-        spinner.innerHTML="<img src='spinner.gif'>";
-        spinner.hidden=false;
-        xhr.send(JSON.stringify(persona));
-       
-    } 
-
-function gestionarValidarUsuario() {
-
-    var div = document.getElementById('respuesta');
-    
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {        
-            var respuesta=(JSON.parse(xhr.responseText));     
-            spinner.hidden=true;       
-            if(respuesta.type === 'error')
-            { 
-                spinner.hidden=true;
-                div.innerHTML="Debe ingresar un email y un password valido";
-                document.getElementById("txtEmail").style.borderColor = 'red';
-                document.getElementById("txtContrasenia").style.borderColor = 'red';           
-                return;
-             
-                     
-            } else if(respuesta.type === 'Admin' || respuesta.type==='User')
+        $.ajax({
+            url: 'http://localhost:3000/login',
+            method: 'post',
+            dataType: 'json',
+            data: persona,
+            beforeSend: function()
             {
-              
-                localStorage.setItem('usuario',respuesta.type );
-                localStorage.setItem('email', emailGlobal);
-                window.location.replace("index.html");
-            }         
-          
-        } else {
+                $('#spinner').show();
+            },
+            success: function(response)
+            {        
+              if(response.type === 'error')
+              {           
+                  $('#spinner').hide();
+                  $('#respuesta').html('Debe ingresar un email y un password valido');                  
+                  $('#txtEmail').css("border-color", "red");
+                  $('#txtContrasenia').css("border-color", "red");                       
+                  return;               
+                       
+              } else if(response.type=== 'Admin' || response.type==='User')
+              {                
+                  localStorage.setItem('usuario',response.type );
+                  localStorage.setItem('email', emailGlobal);
+                  window.location.replace("index.html");
+              }         
 
-            div.innerHTML = "Error: " + xhr.status + " " + xhr.statusText;
-        }
-    } 
-}
-
-window.onload = function()
+            }
+        })  
+    }
+       
+$(document).ready(function()
 {
-    //Boton guardar
-    var btnIngresar = document.getElementById("btnIngresar");
-    //Seteo un evento al btnGuardar
-    btnIngresar.addEventListener('click',function(){
-      ValidarUsuario();      
-    })
+    $('#btnIngresar').click(function()
+    {
+        ValidarUsuario();   
+    });
 
-    var spinner= document.getElementById('spinner');
-
-
-
-   // var spinner= document.getElementById('spinner');
+  $('#spinner').html("<img src='spinner.gif'>").hide();
    
-
-  
-}
+})
 
